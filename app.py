ed7ae8579c68500a33705bb3fb0fc7d67773013d
Anusha Ajayan <aajayan525@gmail.com>
2026-01-28 13:20:24 +0530

    Add Flask API, Docker setup and fixes

--- app.py
+++ app.py
@@ -1,22 +1,25 @@
+from turtle import pd
 from flask import Flask, request, jsonify
 import joblib
+import os
 import pandas as pd
 
 app = Flask(__name__)
 
-# Load trained pipeline model
-model = joblib.load("models/loan_model_v5.pkl")
+MODEL_PATH = "models/loan_model_v5.pkl"  # adjust if needed
+model = joblib.load(MODEL_PATH)
 
-@app.route("/")
+@app.route("/", methods=["GET"])
 def home():
     return "Loan Approval API is Running"
 
 @app.route("/predict", methods=["POST"])
 def predict():
-    data = request.json
-    df = pd.DataFrame([data])
-    prediction = model.predict(df)[0]
-    return jsonify({"loan_status": prediction})
+    data = request.get_json()
+
+    input_df = pd.DataFrame([data])
+    prediction = model.predict(input_df)[0]
+    return jsonify({"loan_status": str(prediction)})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
